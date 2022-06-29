26038716661b44877dd3787cc00311d194075305
akamaki21 <84122175+akamaki21@users.noreply.github.com>
2022-06-29 18:06:43 +0900

    ３５タスクの完了機能実装中 先走って、戻す機能を実装始めたら、超面倒くさいことに気が付いたので一時中断

--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  // alert(inputText);
 
   // DOM生成(div, li, button)
   const div = document.createElement("div");
   div.className = "list-row";
   const li = document.createElement("li");
   li.innerText = inputText;
+  // button
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    // alert("完了");
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    // 押された削除ボタンの親タグ(div)を完了リストに追加して
+    const completeTarget = completeButton.parentNode;
+    const completeText = completeTarget.firstElementChild.innerText;
+    deleteFromIncompetelist(completeTarget);
+
+    // div以下を初期化して、テキストと戻すボタン追加
+    completeTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = completeText;
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻る";
+    returnButton.addEventListener("click", () => {
+      deleteFromCompetelist(returnButton.parentNode);
+      // 未完了リストへ追加＊＊＊なんか面倒くさいことこの上ないんですけど！
+    });
+    completeTarget.appendChild(li);
+    completeTarget.appendChild(returnButton);
+    console.log(completeTarget);
+
+    document.getElementById("complete-list").appendChild(completeTarget);
   });
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    // alert("削除");
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    deleteFromIncompetelist(deleteButton.parentNode);
   });
   div.appendChild(li);
   div.appendChild(completeButton);
@@ -26,6 +47,16 @@ const onClickAdd = () => {
 
   // 未完了TODOへ挿入
   document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompetelist = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 完了リストから指定の要素を削除
+const deleteFromCompetelist = (target) => {
+  document.getElementById("complete-list").removeChild(target);
 };
 
 document
